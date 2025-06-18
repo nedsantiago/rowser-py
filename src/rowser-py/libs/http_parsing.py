2f7fde3391d6e294c459966b4d1792ff62bc0367
Ned Santiago <nedsantiago@proton.me>
2025-06-18 12:42:23 +0800

    Improved type hinting and added an Optional typing class

--- src/rowser-py/libs/http_parsing.py
+++ src/rowser-py/libs/http_parsing.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+from typing import Optional
 
 
 def parse_for_terminal(webpage: str) -> str:
@@ -9,11 +10,17 @@ def parse_for_terminal(webpage: str) -> str:
 
     html_text: str = soup.decode()
 
+    tag_title: Optional[Tag] = soup.title
+    if tag_title is not None:
+        title: str = str(tag_title.name)
+    else:
+        title = "[No Title]"
+
     output_text: str = ""
-    output_text = output_text + f"{soup.title.name=}\n"
+    output_text += f"{title}\n"
     # output_text = output_text + f"{soup.find_all("link")=}\n"
-    output_text = output_text + f"{soup.find_all("span")=}\n"
-    output_text = output_text + f"{soup.find_all("form")=}\n"
+    output_text += f"{soup.find_all("span")=}\n"
+    output_text += f"{soup.find_all("form")=}\n"
 
     return output_text
 
